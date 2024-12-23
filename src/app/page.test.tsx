ff941e0f6c4f8f6806a722f463ad95b6ddb6056b
Ashank Sundaram <ashanksundaram2@gmail.com>
2024-12-23 04:37:53 +0000

    add Jest configuration and initial test for Page component

---
+++ src/app/page.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Page from './page';
+
+describe('Page Component', () => {
+    it('renders the page correctly', () => {
+        render(<Page />);
+        expect(screen.getByText(/some text/i)).toBeInTheDocument();
+    });
+});
