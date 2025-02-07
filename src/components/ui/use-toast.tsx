9e61e6b028b820d78cd02a0b4a02bf7ce4be7640
Ashank <ashank@ashank.tech>
2025-02-07 17:12:03 +0800

    Remove unused files and components to clean up the codebase

--- src/components/ui/use-toast.tsx
+++
@@ -1,6 +0,0 @@
-
-"use client"
-
-import { useToast as useToastOriginal } from "./toast"
-
-export const useToast = useToastOriginal
--- src/tests/__mocks__/toast.tsx
+++
@@ -1,9 +0,0 @@
-const toastFn = jest.fn();
-
-export const useToast = () => {
-  return {
-    toast: toastFn
-  };
-};
-
-export const toast = toastFn;
